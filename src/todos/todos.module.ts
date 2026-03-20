38c12b9bee663a016a7ff8783707317484f4b4bf
Sumit Patel <sumitpatel.14may@gmail.com>
2026-03-20 17:09:45 +0000

    chore: configure axios timeout for external API calls (#8)
    Set 5s timeout and max 3 redirects on HttpModule to prevent
    hanging requests to dummyjson.com.

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -4,7 +4,12 @@ import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 3,
+    }),
+  ],
   controllers: [TodosController],
   providers: [TodosService],
 })
